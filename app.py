869950240a73d920b93306dc85232ee40dc4f8fb
Athenavar <163312751+Athenavar@users.noreply.github.com>
2026-04-09 10:56:29 +0530

    Add functionality to find the greatest of three numbers

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("hello from docker")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+
+greatest = max(num1, num2, num3)
+
+print(f"The greatest number is {greatest}")
+
