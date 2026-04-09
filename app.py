bb1c880709c1e821d2ef37c5fbdc9c351f959a6a
Athenavar <163312751+Athenavar@users.noreply.github.com>
2026-04-09 10:59:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = float(input("Enter third number: "))
+a = 12
+b = 45
+c = 30
 
-
-greatest = max(num1, num2, num3)
-
-print(f"The greatest number is {greatest}")
-
+greatest = max(a, b, c)
+print("The greatest number is:", greatest)
